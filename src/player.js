2c1b5e6aee47784a6d301873ee28955ff8d72dcc
Liliana Weimer <lilianaweimer11@gmail.com>
2020-04-17 18:35:07 -0600

    Create initial player class and methods

---
+++ src/player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(id, token, wins) {
+    this.id = id;
+    this.token = token;
+    this.wins = [];
+    //double check if/how wins should be passed in as parameter!
+  }
+
+  // saveWinsToStorage() {
+  //
+  // }
+  //
+  // retrieveWinsFromStorage() {
+  //
+  // }
+}
