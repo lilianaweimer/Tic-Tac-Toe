28fb1ced186cf707de65956b6f380ceb9c3ca6ff
Liliana Weimer <lilianaweimer11@gmail.com>
2020-04-20 15:21:07 -0600

    Change gameboard layout to divs and make game fully functional on DOM

--- src/game.js
+++ src/game.js
@@ -1,15 +1,15 @@
 class Game {
   constructor(playerOne, playerTwo) {
     this.gameboard = {
-      A1: 0,
-      A2: 0,
-      A3: 0,
-      B1: 0,
-      B2: 0,
-      B3: 0,
-      C1: 0,
-      C2: 0,
-      C3: 0
+      A1: {num: 0, player: undefined},
+      A2: {num: 0, player: undefined},
+      A3: {num: 0, player: undefined},
+      B1: {num: 0, player: undefined},
+      B2: {num: 0, player: undefined},
+      B3: {num: 0, player: undefined},
+      C1: {num: 0, player: undefined},
+      C2: {num: 0, player: undefined},
+      C3: {num: 0, player: undefined}
     };
     this.playerOne = playerOne;
     this.playerTwo = playerTwo;
@@ -26,20 +26,21 @@ class Game {
   }
 
   changeGameboardTile(tile) {
-    this.gameboard[tile] = this.currentPlayer.id;
+    this.gameboard[tile].num = this.currentPlayer.id;
+    this.gameboard[tile].player = this.currentPlayer.token;
   }
 
   checkWinConditions() {
     var board = this.gameboard;
     var winConditions = [
-      (board.A1 + board.A2 + board.A3),
-      (board.B1 + board.B2 + board.B3),
-      (board.C1 + board.C2 + board.C3),
-      (board.A1 + board.B1 + board.C1),
-      (board.A2 + board.B2 + board.C2),
-      (board.A3 + board.B3 + board.C3),
-      (board.A1 + board.B2 + board.C3),
-      (board.A3 + board.B2 + board.C1)
+      (board.A1.num + board.A2.num + board.A3.num),
+      (board.B1.num + board.B2.num + board.B3.num),
+      (board.C1.num + board.C2.num + board.C3.num),
+      (board.A1.num + board.B1.num + board.C1.num),
+      (board.A2.num + board.B2.num + board.C2.num),
+      (board.A3.num + board.B3.num + board.C3.num),
+      (board.A1.num + board.B2.num + board.C3.num),
+      (board.A3.num + board.B2.num + board.C1.num)
     ];
 
     for (var i = 0; i < winConditions.length; i++) {
@@ -60,7 +61,7 @@ class Game {
 
   detectDraw() {
     var board = this.gameboard;
-    var wholeBoard = (board.A1 + board.A2 + board.A3 + board.B1 + board.B2 + board.B3 + board.C1 + board.C2 + board.C3);
+    var wholeBoard = (board.A1.num + board.A2.num + board.A3.num + board.B1.num + board.B2.num + board.B3.num + board.C1.num + board.C2.num + board.C3.num);
     if (wholeBoard === 45 && this.winner === undefined) {
       this.winner = "nobody";
     }
@@ -79,15 +80,15 @@ class Game {
   resetGameBoard() {
     if (this.winner !== undefined) {
       this.gameboard = {
-        A1: 0,
-        A2: 0,
-        A3: 0,
-        B1: 0,
-        B2: 0,
-        B3: 0,
-        C1: 0,
-        C2: 0,
-        C3: 0
+        A1: {num: 0, player: undefined},
+        A2: {num: 0, player: undefined},
+        A3: {num: 0, player: undefined},
+        B1: {num: 0, player: undefined},
+        B2: {num: 0, player: undefined},
+        B3: {num: 0, player: undefined},
+        C1: {num: 0, player: undefined},
+        C2: {num: 0, player: undefined},
+        C3: {num: 0, player: undefined}
       };
     }
   }
--- src/main.js
+++ src/main.js
@@ -7,17 +7,20 @@ var boardHeading = document.querySelector(".gameboard-header-text");
 var board = document.querySelector(".gameboard");
 var gameTile = document.querySelector(".gameboard-tile");
 var playerOneWins = document.getElementById("player-one-wins-grid")
-var playerTwoWins = document.getElementById("player-two-wins-grid")
+var playerTwoWins = document.getElementById("player-two-wins-grid");
+var images = document.querySelectorAll("img");
 
 //EVENT LISTENERS
 board.addEventListener("click", takeTurn);
 
 //EVENT HANDLERS
 function takeTurn() {
-  if (event.target.className = ".gameboard-tile" && event.target.src === "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/spider-web_1f578.png") {
+  if (event.target.className = ".gameboard-tile-div" && event.target) {
     var tile = event.target.getAttribute("data-id");
     event.target.closest(".gameboard-tile");
-    event.target.src = newGame.currentPlayer.token;
+    event.target.innerHTML = `
+    <img src="${newGame.currentPlayer.token}" class="gameboard-tile" id="A1" data-id="A1" alt="${newGame.currentPlayer.name}"/>
+    `;
     newGame.changeGameboardTile(tile);
     checkForWin();
     startNewTurn();
@@ -44,6 +47,19 @@ function checkForWin() {
 }
 
 function startNewRound() {
+  newGame.winner = undefined;
+  newGame.resetGameBoard();
+  board.innerHTML = `
+  <div class="gameboard-tile-div" id="A1" data-id="A1"></div>
+  <div class="gameboard-tile-div" id="A2" data-id="A2"></div>
+  <div class="gameboard-tile-div" id="A3" data-id="A3"></div>
+  <div class="gameboard-tile-div" id="B1" data-id="B1"></div>
+  <div class="gameboard-tile-div" id="B2" data-id="B2"></div>
+  <div class="gameboard-tile-div" id="B3" data-id="B3"></div>
+  <div class="gameboard-tile-div" id="C1" data-id="C1"></div>
+  <div class="gameboard-tile-div" id="C2" data-id="C2"></div>
+  <div class="gameboard-tile-div" id="C3" data-id="C3"></div>
+  `;
 }
 
 function displayWins(winner) {
